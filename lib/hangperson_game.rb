c7ddc5a79e68b1438d4a7b328959c284a0d0e02c
Richard <roibcell@gmail.com>
2016-01-05 08:09:54 +0000

    Finish Part 1: Hangperson implementation

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,10 +8,58 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_reader :word
+  attr_accessor :guesses, :wrong_guesses
+  
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  def guess(letters)
+    raise ArgumentError if letters.nil?
+    raise ArgumentError if letters.empty?
+    letters.split('').each do |letter|
+      letter = letter.downcase
+      raise ArgumentError unless letter =~ /[a-z]/ 
+      
+      if @word.include? letter 
+        if @guesses.include? letter
+          return false
+        else
+          @guesses << letter 
+        end
+      else
+        if @wrong_guesses.include? letter
+          return false
+        else
+          @wrong_guesses << letter 
+        end
+      end
+    end
+    return true
+  end
+  
+  def word_with_guesses
+    result = ''
+    @word.split('').each do |x|
+      if @guesses.include? x
+        result << x
+      else
+        result << '-'
+      end
+    end
+    result
+  end
+  
+  def check_win_or_lose
+    if word.split('').sort == guesses.split('').sort; return :win; end
+    if wrong_guesses.length == 7; return :lose; end
+    :play
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
